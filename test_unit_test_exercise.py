a9d9a466315e6910a2a54cd9adf1915f1be105a0
Kyle Mitra <kjmitra@yahoo.com>
2020-09-02 16:50:13 -0400

    Completed additional work

--- test_unit_test_exercise.py
+++ test_unit_test_exercise.py
@@ -9,3 +9,12 @@ def test_line_maker(x1, y1, x2, y2, x, y):
     from unit_test_exercise import line_maker
     ans = line_maker(x1, y1, x2, y2, x)
     assert y == ans
+
+@pytest.mark.parametrize("x1, y1, x2, y2, x, y", [
+    (0, 0, 5, 5, 1, 1, True),
+    (0, 0, 5, 5, 1, 9, True),
+])
+def test_point_tester(x1, y1, x2, y2, x, y, expected):
+    from unit_test_exercise import point_tester
+    ans = point_tester(x1, y1, x2, y2, x, y)
+    assert expected == ans
--- unit_test_exercise.py
+++ unit_test_exercise.py
@@ -3,3 +3,12 @@ def line_maker(x1, y1, x2, y2, x):
     b = y2 - (slope*x2)
     y = (slope*x) + b
     return y
+
+def point_tester(x1, y1, x2, y2, x3, y3):
+    slope = (y2-y1) / (x2-x1)
+    b = y2 - (slope*x2)
+    y = (slope*x3) + b
+    if y == y3:
+        return True
+    else:
+        return False
